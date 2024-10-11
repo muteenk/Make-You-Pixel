5951ebe96d5f93d5c88e93dfa0ad8bf065f81117
muteenk <kundangarmuteen@gmail.com>
2024-10-11 17:31:24 +0530

    Update Navbar

--- src/app/components/Nav/Navbar.tsx
+++ src/app/components/Nav/Navbar.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
+import {useSession, signOut} from "next-auth/react";
 import Logo from "@/app/components/Atoms/Logo";
 
 const Navbar = ({accModal, setAccModal}) => {
+
+  const {data: session} = useSession();
 
   // Modal States
   const activeState = "fixed rounded-md w-[40%] h-[65vh] bg-[white] z-50 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] animate-modalFadeIn";
@@ -17,9 +21,21 @@ const Navbar = ({accModal, setAccModal}) => {
         <Logo/>
         <div className="flex justify-center items-center">
             <Link href="/" className="font-bold font-sec text-[#232323] text-[15px] mx-7">Explore</Link>
-            <button onClick={toggleModal} className="font-bold font-sec text-[#232323] text-[15px] mx-3">Log In</button>
-            <button onClick={toggleModal} className="font-bold font-sec text-[#232323] text-[15px] mx-3">Join</button>
-        </div>
+            {
+              session ? 
+              <>
+                <button onClick={toggleModal} className="font-bold font-sec text-[#232323] text-[15px] mx-3">
+                  <Image src={session.user.image} alt="User Image" width={30} height={30} className="rounded-full border-[1px] border-yellowBorder" />
+                </button>
+                <button onClick={() => signOut()} className="font-bold font-sec text-[#232323] text-[15px] mx-3">Sign Out</button>
+              </>
+              :
+              <>
+                <button onClick={toggleModal} className="font-bold font-sec text-[#232323] text-[15px] mx-3">Log In</button>
+                <button onClick={toggleModal} className="font-bold font-sec text-[#232323] text-[15px] mx-3">Join</button>
+              </>
+            }
+            </div>
     </nav>
   )
 }
